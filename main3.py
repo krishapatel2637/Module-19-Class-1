a03742b3ba90fad340ff570f108cafc375bf9f8f
krishapatel2637 <krishapatel260307@gmail.com>
2026-08-25 19:55:36 +0530

    initial commit

---
+++ main3.py
@@ -0,0 +1,17 @@
+n = int(input("Enter a number: "))
+if(n%2==0):
+    print("Even")
+else:
+    print("Odd")
+if(n<=1):
+    print("Not Prime")
+else:
+    prime = True
+    for i in range(2,n):
+        if(n%i==0):
+            prime = False
+            break
+    if prime:
+        print("Prime")
+    else:
+        print("Not Prime")
